d8e6cbfca3f0576fe3c818b4f9f6f0bb3ee64349
Hikmat Samadov <s3m3dov@hotmail.com>
2023-03-04 10:28:24 +0400

    feat: create product model

--- apps/product/models.py
+++ apps/product/models.py
@@ -1,3 +1,15 @@
+import uuid as uuid_lib
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    uuid = models.UUIDField(primary_key=True, default=uuid_lib.uuid4, editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    logo = models.URLField(blank=True, null=True)
+    rotate_duration = models.FloatField(null=True)
+
+    def __str__(self):
+        return self.name
