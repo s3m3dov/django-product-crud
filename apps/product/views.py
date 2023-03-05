acce409105fb4f8b8370a45c2e6653d57bb85b1e
Hikmat Samadov <s3m3dov@hotmail.com>
2023-03-05 16:36:40 +0400

    feat: refactor views & call ImageUploadTask inside product-create view

--- apps/product/views.py
+++ apps/product/views.py
@@ -1,61 +1,88 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.urls import reverse
+import base64
+import logging
 
-from .forms import ProductForm
-from .models import Product
+from django.http import HttpResponseRedirect
+from django.shortcuts import (
+    get_object_or_404,
+    redirect,
+    render,
+)
+from django.urls import reverse_lazy, reverse
+from django.views import View
+from django.views.generic import ListView
+
+from apps.product.forms import ProductForm
+from apps.product.models import Product
+from apps.product.tasks import ImageUploadTask
+from apps.product.utils import generate_unique_filename
+
+logger = logging.getLogger(__name__)
 
 
-# Create your views here.
+class ProductListView(ListView):
+    model = Product
+    template_name = "product-list.html"
+    context_object_name = "products"
+    paginate_by = 10
 
-def productListView(request):
-    products = Product.objects.all()
-    return render(request, "product-list.html", {'products': products})
+
+class ProductCreateView(View):
+    form_class = ProductForm
+    template_name = "product-create.html"
+    success_url = reverse_lazy("product:product-list")
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST, request.FILES)
+
+        if form.is_valid():
+            logo_file = form.cleaned_data.get("logo_file")
+            filename = generate_unique_filename(logo_file.name)
+            encoded_file = base64.b64encode(logo_file.read()).decode("utf-8")
+            data = {"file": encoded_file, "name": filename}
+
+            try:
+                product = form.save()
+                ImageUploadTask().delay(product.uuid, data)
+                return redirect(self.success_url)
+            except Exception as e:
+                logger.error(e)
+
+        return render(request, self.template_name, {"form": form})
 
 
-def productCreateView(request):
-    if request.method == "POST":
-        form = ProductForm(request.POST)
+class ProductView(View):
+    template_name = "product.html"
+
+    def get(self, request, pk):
+        product = get_object_or_404(Product, uuid=pk)
+        return render(request, self.template_name, {"product": product})
+
+
+class ProductUpdateView(View):
+    form_class = ProductForm
+    template_name = "product-update.html"
+    success_url = reverse_lazy("product:product-list")
+
+    def get(self, request, pk):
+        product = get_object_or_404(Product, uuid=pk)
+        form = self.form_class(instance=product)
+        return render(request, self.template_name, {"form": form, "product": product})
+
+    def post(self, request, pk):
+        product = Product.objects.get(uuid=pk)
+        form = ProductForm(request.POST, request.FILES, instance=product)
         if form.is_valid():
             try:
                 form.save()
-                return redirect(reverse('product:product-list'))
+                return redirect(self.success_url)
             except Exception as e:
-                print("Error: ", e)
-    else:
-        form = ProductForm()
-    return render(request, 'product-create.html', {'form': form})
+                logger.error(e)
+        return render(request, self.template_name, {"form": form, "product": product})
 
-
-def productView(request, pk):
-    product = Product.objects.get(uuid=pk)
-    return render(request, 'product.html', {'product': product})
-
-
-def productUpdateView(request, pk):
-    product = Product.objects.get(uuid=pk)
-    print("Product: ", product)
-    form = ProductForm(request.POST, instance=product)
-    if form.is_valid():
-        form.save()
-        return redirect(reverse('product:product-list'))
-    return render(request, 'product-update.html', {'form': form})
-
-
-def productUpdateView(request, pk):
-    product = get_object_or_404(Product, uuid=pk)
-    if request.method == "POST":
-        form = ProductForm(request.POST, instance=product)
-        if form.is_valid():
-            form.save()
-            # messages.success(request, f"{product.name} has been updated successfully!")
-            return redirect(reverse('product:product-list'))
-        else:
-            # messages.error(request, "Failed to update product. Please correct the errors below.")
-            print("Error: ", form.errors)
-    else:
-        form = ProductForm(instance=product)
-
-    return render(request, 'product-update.html', {'form': form})
 
 
 def productDeleteView(request, pk):
