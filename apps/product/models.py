76a87426afc6a5bfaa8420f0a05a993581aacdf8
Hikmat Samadov <s3m3dov@hotmail.com>
2023-03-05 18:52:15 +0400

    feat: add modified field to the model

--- apps/product/models.py
+++ apps/product/models.py
@@ -11,6 +11,7 @@ class Product(models.Model):
     updated = models.DateTimeField(auto_now=True)
     logo = models.URLField(blank=True, null=True)
     rotate_duration = models.FloatField(null=True)
+    modified = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
