1a9e3b02561abc8183e411c02a7b30576e9e48b1
Hikmat Samadov <s3m3dov@hotmail.com>
2023-03-05 18:31:52 +0400

    feat: add validation for checking file type on upload

--- apps/product/forms.py
+++ apps/product/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 from apps.product.models import Product
-from config.validators import validate_file_size
+from config.validators import validate_file_size, validate_file_type
 
 
 class ProductForm(forms.ModelForm):
@@ -10,4 +10,4 @@ class ProductForm(forms.ModelForm):
         fields = "__all__"
         exclude = ["uuid", "created", "updated", "logo", "rotate_duration"]
 
-    logo_file = forms.FileField(required=True, validators=[validate_file_size])
+    logo_file = forms.FileField(required=True, validators=[validate_file_size, validate_file_type])
